01d6198bf3145e3c61cdc997f46e4b184dfd2936
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2023-12-21 11:48:20 +0100

    build ios without haven

--- scripts/ios/build_sodium.sh
+++ scripts/ios/build_sodium.sh
@@ -10,6 +10,7 @@ echo "============================ SODIUM ============================"
 echo "Cloning SODIUM from - $SODIUM_URL"
 git clone $SODIUM_URL $SODIUM_PATH --branch stable
 cd $SODIUM_PATH
+git checkout 443617d7507498f7477703f0b51cb596d4539262
 ./dist-build/ios.sh
 
 mv ${SODIUM_PATH}/libsodium-ios/include/* $EXTERNAL_IOS_INCLUDE_DIR
